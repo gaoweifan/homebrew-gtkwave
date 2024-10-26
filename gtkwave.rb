1001ea54f363c3d1530716e9722b9b40ebd8a5a1
Yan-Jie Wang <ubzeme@gmail.com>
2024-10-26 10:25:02 +0800

    Update to use lts version

--- gtkwave.rb
+++ gtkwave.rb
@@ -2,23 +2,29 @@ class Gtkwave < Formula
   desc "GTKWave"
   homepage "https://gtkwave.sourceforge.net"
   license "GPL-2.0-or-later"
-  head "https://github.com/randomplum/gtkwave.git", branch: "master"
-  revision 3
+  url "https://gtkwave.sourceforge.net/gtkwave-gtk3-3.3.121.tar.gz"
+  sha256 "54aa45788d6d52afb659c3aef335aafde0ef2c8990a7770f8eaa64e57f227346"
 
-  depends_on "desktop-file-utils" => :build # for update-desktop-database
-  depends_on "shared-mime-info" => :build
   depends_on "gobject-introspection" => :build
-  depends_on "gtk-mac-integration" => :build
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "gtk+3"
+  depends_on "xz"
+  depends_on "tcl-tk"
+
+  on_macos do
+    depends_on "gtk-mac-integration"
+    patch :p2 do
+      url "https://github.com/yanjiew1/gtkwave/compare/0704899dd44467395d24ee78bb3d819fc10540a6...f3a85609fec4bb10f8009bcdf1a68abec03eb997.patch"
+      sha256 "4d4c730777cb4f0a243387b8793e0230ea68d3ac8368c61118bbb984c096117a"
+    end
+  end
 
   def install
-    ENV["DESTDIR"] = "/"
-    system "meson", "setup", "build", "-Dintrospection=false", *std_meson_args
-    system "meson", "compile", "-C", "build", "--verbose"
-    system "meson", "install", "-C", "build"
+    system "./configure", "--enable-gtk3",
+                          "--with-tcl=#{Formula["tcl-tk"].opt_prefix}/lib",
+                          "--with-tk=#{Formula["tcl-tk"].opt_prefix}/lib",
+                          *std_configure_args
+    system "make", "install"
   end
 
 end
